ab2345c8f9ac741967bc364c7b39f72636232101
facobackup <gustavomicaelbarbosa@gmail.com>
2023-10-14 19:42:03 -0300

    [STD-31] Fixing document test

--- test/ui/DocumentTest.cpp
+++ test/ui/DocumentTest.cpp
@@ -4,59 +4,39 @@
 #include "../../src/ui/core/Document.h"
 #include "Example.h"
 
-//TEST_CASE("Should add element", "[document-add]") {
-//    Catalyst::Document document;
-//    document.addElement<Test>("c", nullptr);
-//    REQUIRE(document.getElements().getLength() == 1);
-//}
-//
-//TEST_CASE("Should add child", "[document-add-child]") {
-//    Catalyst::Document document;
-//    Test *created = document.addElement<Test>("c", nullptr);
-//    document.addElement<Test>("2", created);
-//    Catalyst::IElement *found = document.getElementById("c");
-//    REQUIRE(created->getChildren()->getLength() == 1);
-//    REQUIRE(document.quantityOfElements() == 2);
-//    REQUIRE(found == created);
-//}
-//
-//TEST_CASE("Should find deep child", "[document-getbyid]") {
-//    Catalyst::Document document;
-//    Test *created1 = document.addElement<Test>("1", nullptr);
-//    Test *created2 = document.addElement<Test>("2", created1);
-//    Test *created3 = document.addElement<Test>("3", created2);
-//    Test *created4 = document.addElement<Test>("4", created3);
-//    Test *created5 = document.addElement<Test>("5", created4);
-//
-//    Catalyst::IElement *found5 = document.getElementById("5");
-//    Catalyst::IElement *found4 = document.getElementById("4");
-//    Catalyst::IElement *found3 = document.getElementById("3");
-//    Catalyst::IElement *found2 = document.getElementById("2");
-//    Catalyst::IElement *found1 = document.getElementById("1");
-//
-//    REQUIRE(found5 == created5);
-//    REQUIRE(found4 == created4);
-//    REQUIRE(found3 == created3);
-//    REQUIRE(found2 == created2);
-//    REQUIRE(found1 == created1);
-//}
-
-
-
 namespace Catalyst::DocumentTest {
     Document d;
+
     void loadView() {
         d.addView<Example>();
+        List<IElement> *elements = d.getElementsState()->getElements();
 
-        REQUIRE(d.getElementsState()->getElements()->getLength() == 3);
+        IElement *first = elements->getFirstValue();
+        IElement *second = first->getChildren()->getFirstValue();
+        IElement *third = second->getChildren()->getFirstValue();
+
+        REQUIRE(elements->getLength() == 1);
+        REQUIRE(first->getChildren()->getLength() == 1);
+        REQUIRE(second->getChildren()->getLength() == 1);
+        REQUIRE(second->getId() == "SECTION");
+        REQUIRE(third->getChildren()->getLength() == 0);
+        REQUIRE(third->getId() == "TEXT");
+    }
+
+    void findDeepChild() {
+        ElementsState *state = d.getElementsState();
+        List<IElement> *elements = state->getElements();
+        IElement *first = elements->getFirstValue();
+        IElement *second = first->getChildren()->getFirstValue();
+        IElement *third = second->getChildren()->getFirstValue();
+        REQUIRE(state->getElementById("SECTION") == second);
+        REQUIRE(state->getElementById("TEXT") == third);
     }
 
     Tester *createTester() {
         auto tester = new Tester("EngineTest");
         tester->registerTest("Should load view", loadView);
-//        tester->registerTest("Should find deep child",);
-//        tester->registerTest("Should add child",);
-//        tester->registerTest("Should add element",);
+        tester->registerTest("Should find deep child", findDeepChild);
         return tester;
     }
 }
